6c137affe72b5fa80dca76bd002604c3e304a407
Fraser Brown <fraserb42@gmail.com>
2015-11-14 11:05:47 +0000

    init project

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(5f * Time.deltaTime, 0f, 0f);
+        transform.T
+	}
+}
