29c5814a8912515c3f22cd36daeba402abff90cf
SirCactus <cactus248@hotmail.co.uk>
2015-11-16 04:09:58 +0000

    Fixed balloons and layering, will fix double layering tomorow!

--- Assets/Scripts/Balloon.cs
+++ Assets/Scripts/Balloon.cs
@@ -1,41 +1,19 @@
-﻿/*using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
 public class Balloon : MonoBehaviour
 {
-    public int numberofballoons;
-    public Transform prefab;
-
-    private Vector2[] Existingballs;
-
     // Use this for initialization
     private void Start()
     {
-        for (int i = 0; i < numberofballoons; i++)
-        {
-            Instantiate(prefab, new Vector2(3, (Camera.current.transform.position.y) - 10), Quaternion.identity);
-
-        }
     }
 
     // Update is called once per frame
     private void Update()
     {
-        for (int i = 0; i < numberofballoons; i++)
-        {
-            // Existingballs[i];
-            [] findobjs = FindObjectsOfType(typeof) as object type[];
-
-
-            {
-                if ((Existingballs[i].y - Camera.current.transform.position.y) > 5)
-                {
-                    Debug.Log("It's out of frame, bring it down.");
-                    Existingballs[i].y = (Existingballs[i].y - 10);
-                }
-            }
+        if ((transform.position.y - Camera.current.transform.position.y) > 7) {
+            transform.Translate((0-transform.position.x)+ Random.Range(-8.0F, 8.0F), -15,0);
         }
     }
 }
-*/
